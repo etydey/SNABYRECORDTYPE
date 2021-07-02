465c17f325d161b5f108b0031ccd69613cf0a3e9
Ety Dey <Ety.Dey@albertahealthservices.ca>
2021-07-02 16:11:26 -0600

    date format test

--- app.js
+++ app.js
@@ -329,7 +329,7 @@ const d3Tableau = () => {
 		}
 
 		function handleMouseOver(node) {
-			var datevalue = dateConvert(node.Variant_Identified_Date);
+			var datevalue = dateConvert(node.Variant_Identified_Date).toDateString();
 			var htmlContent = "<div>";
 			htmlContent += "Variant: " + node.VARIANT + "<br>";
 			htmlContent +=
