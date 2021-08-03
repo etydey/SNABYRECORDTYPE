ccba223ff743d8c435cab182629b2e1a05840343
Ety Dey <Ety.Dey@albertahealthservices.ca>
2021-08-03 11:21:17 -0600

    updated code

--- app.js
+++ app.js
@@ -236,7 +236,7 @@ const d3Tableau = () => {
 			//	EXIT
 				link.exit().remove();
 			//	ENTER
-			var newlink = g
+			var newlink = link
 				.append("g")
 				.attr("class", "links")
 				.selectAll("line")
@@ -251,7 +251,7 @@ const d3Tableau = () => {
 			//	EXIT
 			node.exit().remove();
 			//	ENTER
-			var newnode = g
+			var newnode = node
 				.append("g")
 				.attr("class", "nodes")
 				.selectAll("circle")
